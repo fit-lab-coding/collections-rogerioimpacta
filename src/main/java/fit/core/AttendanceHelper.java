c6df4e02ef7b94cc91ce627b490cc80c1ed2ece6
Rogério Limas Moreira <rogerio.moreira@visto.global>
2021-02-08 21:26:44 -0300

    Correção do classroom.yml

--- src/main/java/fit/core/AttendanceHelper.java
+++ src/main/java/fit/core/AttendanceHelper.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AttendanceHelper {
-    public static Map<String, Boolean> random(int amount, int percentage) {
+    public static Map<String, Boolean> random2(int amount, int percentage) {
         int result = (amount * percentage) / 100;
 
         Map<String, Boolean> listUser = new HashMap<>();
@@ -19,5 +19,18 @@ public class AttendanceHelper {
 
 
         return listUser;
+    }
+
+    public static Map<String, Boolean> random(int quantity, int presentPercentile) {
+        Map<String, Boolean> attendanceList = new HashMap<>();
+
+        int totalPresence = (int)Math.ceil(quantity * (presentPercentile / 100.00));
+
+        for (int i = 0; i < totalPresence; i++) {
+            boolean present = i < totalPresence;
+            attendanceList.put("" + i, present);
+        }
+
+        return attendanceList;
     }
 }
